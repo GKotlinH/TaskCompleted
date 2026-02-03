20a90a9612d55a0ece4813846deef1d6894174b2
Git <2924734295@qq.com>
2026-02-03 13:24:25 +0800

    添加 innerPadding 处理

--- app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskCompletedTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TaskCompletedScreen()
+                    TaskCompletedScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -39,9 +39,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TaskCompletedScreen() {
+fun TaskCompletedScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .fillMaxHeight(),
         verticalArrangement = Arrangement.Center,
